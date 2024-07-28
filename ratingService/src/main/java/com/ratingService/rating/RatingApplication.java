bbc37f746b479a8f2660af30e657d904f1225c68
aryan kumar <aryankumarpandito@gmail.com>
2024-07-28 22:32:12 +0530

    eureka client configured

--- ratingService/src/main/java/com/ratingService/rating/RatingApplication.java
+++ ratingService/src/main/java/com/ratingService/rating/RatingApplication.java
@@ -2,8 +2,10 @@ package com.ratingService.rating;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RatingApplication {
 
 	public static void main(String[] args) {
--- userService/src/main/java/com/user/service/UserServiceApplication.java
+++ userService/src/main/java/com/user/service/UserServiceApplication.java
@@ -2,8 +2,12 @@ package com.user.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+//@PropertySource("classpath:application.yml")
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
